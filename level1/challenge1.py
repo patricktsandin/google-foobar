3f8e235c69dcc92c6e8bbfbcafbf31258a2c80b8
Patrick Sandin <patrick@sandin.tech>
2023-08-10 19:28:11 -0700

    Adds comment.

--- level1/challenge1.py
+++ level1/challenge1.py
@@ -1,4 +1,9 @@
 def solution(data, n):
+    """
+    Given a list of ID numbers and a limit, return the list without the ID
+    numbers which appear more often than the limit.
+    solution([1, 1, 2, 3, 3, 3, 4, 5], 2) --> [2, 4, 5]
+    """
     counter = {}
     for id_number in data:
         if id_number not in counter:
