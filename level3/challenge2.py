bc1f5319b687dbda3af47b6fc3345f20e8b26079
Patrick Sandin <patrick@sandin.tech>
2023-08-12 21:41:24 -0700

    Adds work-in-progress implementation for Google FooBar level 3, challenge 2.

--- level3/challenge2.py
+++ level3/challenge2.py
@@ -11,5 +11,77 @@ The task is to calculate the probabilities of terminating in each terminal
 position.
 """
 
-def solution(m):
-    pass  # TODO: research probabilistic automata and Markov chains tomorrow.
+from decimal import Decimal
+from fractions import Fraction
+
+m = [
+    [0, 1, 0, 0, 0, 1],
+    [4, 0, 0, 3, 2, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+]
+
+
+def get_stable_states(samples):
+    stable_states = []
+    for sample in samples:
+        is_stable = sum(sample) == 0
+        stable_states.append(is_stable)
+    return stable_states
+
+
+def convert_to_decimal(samples):
+    for sample in samples:
+        denominator = sum(sample)
+        for transition, probability in enumerate(sample):
+            if denominator == 0:
+                probability = Decimal(0)
+            else:
+                probability = Decimal(
+                    Decimal(probability) / Decimal(denominator)
+                )
+            sample[transition] = probability
+    return samples
+
+
+def solution(samples):
+    samples = convert_to_decimal(samples)
+    iterations = 10
+    queue = [(1, 0)]
+    stable_states = get_stable_states(samples)
+    probabilities = [[]]*len(samples)
+
+    # import pdb; pdb.set_trace()
+    while iterations > 0:
+        print ''
+        print ''
+        iterations -= 1
+        print "Iterations:", iterations
+        print "Queue", queue
+        previous_probability, state = queue.pop(0)
+        print "Prev prob:", previous_probability, "Prev state:", state
+        sample_id = state
+        print "Sample:", samples[sample_id]
+        for transition in range(len(samples[sample_id])):
+            print "This prob from record:", probabilities[transition]
+            print "This prob from sample:", samples[sample_id][transition]
+            import pdb; pdb.set_trace()
+            if samples[sample_id][transition]:
+                if probabilities[transition]:
+                    probabilities[transition].append(
+                        probabilities[transition][-1] * previous_probability
+                    )
+                else:
+                    probabilities[transition].append(
+                        samples[sample_id][transition] * previous_probability
+                    )
+            if not stable_states[transition] and samples[sample_id][transition] != 0:
+                queue.append((probabilities[transition][-1], transition))
+            print "Prob record:", probabilities
+
+    print probabilities
+
+
+solution(m)
